77a6fbd7ee8286f219564c5e955379457a6045cf
Will Moffat <will@hamstersoup.com>
2014-10-20 12:33:11 +0200

    Added /etc/rc.local

--- link.sh
+++ link.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 set -eu
 
+sudo cp -i rc.local /etc/rc.local
+
 cd $HOME
 FILES=$(find .dotfiles -maxdepth 1 -not -name '*.git' -iwholename '.dotfiles/.*' -print)
 ln -si $FILES $HOME 
+
