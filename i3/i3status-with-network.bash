8d6c64ceac76cfb586e635b4a7f8ee771969098b
Will Moffat <will@hamstersoup.com>
2016-05-19 07:06:50 +0200

    i3 better directory layout.

--- i3/i3status-with-network.bash
+++ i3/i3status-with-network.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 set -eu
+cd "$(dirname "$0")"
 
-i3status -c ~/.dotfiles/i3-wm-status.conf | (
+i3status -c i3status.conf | (
     read line && echo "$line" && while :
         do
             read line
-            dat=$(~/.dotfiles/bin/measure-net-speed.bash)
+            dat=$(./measure-net-speed.bash)
             dat="[{ \"full_text\": \"${dat}\" },"
             echo "${line/[/$dat}" || exit 1
         done)
--- link.sh
+++ link.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 set -eu
 
-pushd $HOME
-FILES=$(find .dotfiles -maxdepth 1 -not -name '*.git' -iwholename '.dotfiles/.*' -print)
-ln -si $FILES $HOME 
-
+pushd "$HOME/.i3"
+ln -si ~/.dotfiles/i3/config
 popd
+
+pushd "$HOME"
+FILES=$(find .dotfiles -maxdepth 1 -not -name '*.git' -iwholename '.dotfiles/.*' -print)
+ln -si "$FILES" "$HOME"
+popd
+
 echo "Press ENTER to overwrite Chromebook /etc/rc.local"
-read dummy
+read
 sudo cp -i rc.local /etc/rc.local
