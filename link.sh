def9755b8aa9dd342b13faed0d59094fecb30dd3
Will Moffat <will@hamstersoup.com>
2017-02-21 17:20:16 +0100

    Fix link.sh setup script.

--- link.sh
+++ link.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 set -eu
 
+mkdir "$HOME/.i3"
 pushd "$HOME/.i3"
 ln -si ~/.dotfiles/i3/config
 popd
 
 pushd "$HOME"
 FILES=$(find .dotfiles -maxdepth 1 -not -name '*.git' -iwholename '.dotfiles/.*' -print)
-ln -si "$FILES" "$HOME"
+for FILE in "$FILES"; do
+  ln -si "$FILE" "$HOME"
+done
 popd
 
 echo "Remember to manually copy files in machines/"
