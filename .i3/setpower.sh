980a8c5fdb6d23cd26dc6f24a4cadf737b9fd4ab
Will Moffat <will@muuselabs.com>
2017-03-13 11:19:18 +0100

    keep trying to fix hibernate

--- .i3/setpower.sh
+++ .i3/setpower.sh
@@ -7,11 +7,15 @@ i3lock -c 990000
 
 # Sometimes it's not possible to restart after this command:
 # systemctl suspend
+# Maybe because of https://bugzilla.kernel.org/show_bug.cgi?id=104771
+#   Fixed in https://github.com/torvalds/linux/commit/406f992
+#   Kernel 4.7
 
 # Note(wdm) This requires hackery such as:
 # sudo chmod 666 /sys/power/state
-echo $1 > /sys/power/state
+# echo $1 > /sys/power/state
 
+systemctl $1
 # Note(wdm) Capslock as Control. TODO(wdm) Why do I need this again?
 sleep 3
 setxkbmap -option ctrl:nocaps
